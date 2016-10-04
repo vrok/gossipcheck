51ec1102ef2c16110bf386b7cda0048447721044
Marcin Wrochniak <wrochniak@gmail.com>
2016-10-04 20:05:34 +0200

    Added process running checker

--- checks/checks.go
+++ checks/checks.go
@@ -3,9 +3,12 @@ package checks
 import (
 	"encoding/gob"
 	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -176,9 +179,54 @@ type procRunningCheck struct{}
 
 func (fc procRunningCheck) Type() CheckType { return CheckProcRunning }
 
+var num = regexp.MustCompile("^\\d+$")
+
+func fetchProcessExeAndArgs(pid string) (exe string, args string, err error) {
+	exe, err = os.Readlink(fmt.Sprintf("/proc/%s/exe", pid))
+	if err != nil {
+		return "", "", fmt.Errorf("Couldn't read link for pid %d: %s", pid, err)
+	}
+
+	raw, err := ioutil.ReadFile(fmt.Sprintf("/proc/%s/cmdline", pid))
+	if err != nil {
+		return "", "", fmt.Errorf("Couldn't read arguments for pid %d: %s", pid, err)
+	}
+
+	// Items in cmdline files are separated with 0x00 byte
+	cmdline := strings.Join(strings.Split(string(raw), "\x00"), " ")
+	return exe, cmdline, nil
+}
+
 func (fc procRunningCheck) Run(p *Params) error {
-	log.Println("proc running check")
-	return nil
+	files, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if num.MatchString(file.Name()) {
+			exe, args, err := fetchProcessExeAndArgs(file.Name())
+
+			if err != nil {
+				continue
+			}
+
+			failed := false
+			if p.Path != "" && !strings.Contains(exe, p.Path) {
+				failed = true
+			}
+
+			if p.Check != "" && !strings.Contains(args, p.Check) {
+				failed = true
+			}
+
+			if !failed {
+				log.Printf("Found requested process (%s)", file.Name())
+				return nil
+			}
+		}
+	}
+	return errors.New("Didn't find requested process")
 }
 
 // Useful for testing. Always succeeds when check is empty, fails otherwise.
